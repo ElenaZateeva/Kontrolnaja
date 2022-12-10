d2f490ba99293e691aca8154aa639aae9a64698c
ElenaZateeva <zateevaelena14@gmail.com>
2022-12-10 18:19:36 +0300

    Задание строчного массива и вывод на печать

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача. Создать массив сотрок, организовать проверку массива. 
+ Все строки длинной менее 3 симводов скопировать в новый массив строк.*/
+
+ string[] strArray = new string[] {"alfa","betta","gamma","delta","sigma","lda"};
+
+PrintArray(strArray);
+Console.WriteLine();
+
+void PrintArray(string[] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
