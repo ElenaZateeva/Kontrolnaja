670293d8badace1e759848f68b18fe40d5b0c4ed
ElenaZateeva <zateevaelena14@gmail.com>
2022-12-10 19:42:36 +0300

    Создали строку из слов менее 4 символов

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
 ﻿/* Задача. Создать массив сотрок, организовать проверку массива. 
  Все строки длинной менее 3 симводов скопировать в новый массив строк.*/
-
- string[] strArray = new string[] {"alfa","betta","gamma","delta","sigma","lda"};
+Console.WriteLine("Исходный массив слов:");
+string[] strArray = new string[] { "alfa", "min", "betta", "gamma", "max", "delta", "sigma", "lda", "ert", "ql", "col" };
 
 PrintArray(strArray);
-Console.WriteLine();
+string str = Vibor(strArray);
+Console.WriteLine(str);
+
+string Vibor(string[] array)
+{
+    string stroka = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            stroka = stroka + " "+array[i];
+        }
+    }
+    return stroka;
+}
+
 
 void PrintArray(string[] array)
 {
